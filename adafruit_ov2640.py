5ddcbb9115dbf7a25eca291b6bd66138fe9a1a82
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_ov2640.py
+++ adafruit_ov2640.py
@@ -29,7 +29,7 @@ Implementation Notes
 # pylint: disable=too-many-lines
 # imports
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_OV7670.git"
 
 import time
