40f9bc5988a596a14b6a7ebb3b78406cd894d7a5
foamyguy <foamyguy@gmail.com>
2025-05-27 16:40:11 -0500

    displayio API updates

--- examples/ov2640_displayio_kaluga1_3_ili9341.py
+++ examples/ov2640_displayio_kaluga1_3_ili9341.py
@@ -19,6 +19,7 @@ I2C pull-ups(!)
 import board
 import busio
 import displayio
+import fourwire
 from adafruit_ili9341 import ILI9341
 
 import adafruit_ov2640
@@ -29,7 +30,7 @@ import adafruit_ov2640
 displayio.release_displays()
 
 spi = busio.SPI(MOSI=board.LCD_MOSI, clock=board.LCD_CLK)
-display_bus = displayio.FourWire(
+display_bus = fourwire.FourWire(
     spi, command=board.LCD_D_C, chip_select=board.LCD_CS, reset=board.LCD_RST
 )
 display = ILI9341(display_bus, width=320, height=240, rotation=90)
